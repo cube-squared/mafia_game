b1909afdf80bc429b961659043d785bf9710d8b8
WyattH224 <46971631+WyattH224@users.noreply.github.com>
2019-03-26 08:55:03 -0700

    Add files via upload
    Fixed how roles worked

--- game_package/lib/flutter_package.dart
+++ game_package/lib/flutter_package.dart
@@ -1,22 +1,16 @@
 library game_package;
 
-class Player {
-  var team = '';
-  var role = '';
-  var status = "Alive";
-  var saved = false;
+abstract class Player {
+  var team;
+  var role;
   var name;
+  var status;
+  var saved;
 
-  Player(String name) {
-    this.name = name;
-  }
 
-  Player setRole(r) {
-    if (r == 'Doctor') return Doctor(name);
-    if (r == 'Mafia') return Mafia(name);
-    if (r == 'Innocent') return Innocent(name);
-    throw "No Valid Role";
-  }
+
+
+
 
   void setDead(){
     if(saved == false) {
@@ -31,37 +25,54 @@ class Player {
     print("Status: " + status);
   }
 
+
+
+}
+
+class Mafia extends Player {
+
+  Mafia(String name){
+    team = 'Mafia';
+    role = 'Mafia';
+    this.name = name;
+    status = "Alive";
+    saved = false;
+  }
+
   void killPlayer(Player player) {
     if (role == "Mafia") {
       player.setDead();
     }
   }
-
-}
-
-class Mafia extends Player {
-  var team = 'Mafia';
-  var role = 'Mafia';
-  Mafia(name):super(name);
 }
 
 
 class Doctor extends Player{
-  var team = 'Town';
-  var role = 'Doctor';
-  Doctor(name):super(name);
+
+  Doctor(String name){
+    team = 'Town';
+    role = 'Doctor';
+    this.name = name;
+    status = "Alive";
+    saved = false;
+  }
+
 }
 
-class Innocent extends Player {
-  var team = 'Town';
-  var role = 'Innocent';
-  Innocent(name):super(name);
+class Innocent extends Player{
+  Innocent(String name){
+    team = 'Town';
+    role = 'Innocent';
+    this.name = name;
+    status = "Alive";
+    saved = false;
+  }
 }
 
 
 main() {
-  final player1 = Player("Wyatt").setRole('Doctor');
-  final player2 = Player("Talon").setRole('Mafia');
+  final player1 = Doctor("Wyatt");
+  final player2 = Mafia("Matthew");
   player1.displayDetails();
   player2.displayDetails();
   player2.killPlayer(player1);

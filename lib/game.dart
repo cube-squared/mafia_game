630c338bda358f665e55d6666edb6ca8f3138dd6
Talok131 <Talonshepard@gmail.com>
2019-05-20 07:59:33 -0700

    Added the getNarration method

--- lib/game.dart
+++ lib/game.dart
@@ -415,7 +415,9 @@ class Game {
     for (int i = 0; i < Player.allThePlayers.length; i++) {
       Player.allThePlayers[i].displayDetails();
     }
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     print( await GameDatabase.getNarration(Game.partyId, Player.allThePlayers[1].uid, "murder"));
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     //endGame();
 
   }
